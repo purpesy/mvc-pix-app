030b53b17a4ae5f75b0514fca4b9fd1796415ccf
purpesy <lucasgabrieldossantos753@gmail.com>
2025-07-21 19:35:14 -0300

    Fix: Adicionei a rota após logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -62,6 +62,6 @@ class AuthController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }
